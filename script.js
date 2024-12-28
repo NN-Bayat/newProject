6f4621769854054b00f2ab862e4e37d71c1f16cf
NN-Bayat <noorullahbayat1414@gmail.com>
2024-12-28 11:06:20 +0430

    js file add

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuIcon = menuBtn.querySelector("i");
-const navLinks = document.getElementById("nav-links")
+const navLinks = document.getElementById("nav-links");
 
 menuBtn.addEventListener("click", (e) => {
     navLinks.classList.toggle("was");
 
     const wasAst = navLinks.classList.contains("was");
-    menuIcon.setAttribute("class", wasAst ? "ri-close-fill" : "ri-menu-3-line")
+    menuIcon.setAttribute("class", wasAst ? "ri-close-fill" : "ri-menu-3-line");
 });
 
 navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("was");
-})
+});
+
+ScrollReveal().reveal(".header__images img", {
+    distance: "50px",
+    duration: 1000,
+    origin: "right",
+    interval: 500,
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 800,
+});
+
+ScrollReveal().reveal(".section__description", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content form", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".choose__image img", {
+    distance: "50px",
+    duration: 1000,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".choose__content .section__sub_header", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 500,
+});
+
+ScrollReveal().reveal(".choose__content .section__header", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 800,
+});
+
+ScrollReveal().reveal(".choose__list li", {
+    distance: "50px",
+    duration: 1000,
+    origin: "bottom",
+    delay: 1300,
+    interval: 800,
+});
+
+
 
 
 
